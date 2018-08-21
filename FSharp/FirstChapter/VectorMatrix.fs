e8b235ba12ecaf75d25099d4550ef9b6b3276e28
LoveLan1314 <736265181@qq.com>
2018-08-22 00:29:45 +0800

    添加PowerPack包引用

---
+++ FSharp/FirstChapter/VectorMatrix.fs
@@ -0,0 +1,11 @@
+﻿module VectorMatrix
+
+open Microsoft.FSharp.Math
+
+let v1 = vector [1.6; 2.4; 3.2]
+let m1 = matrix [[1.0; 2.0; 1.5]; [2.5; 1.0; 0.0]; [1.0; 0.5; 1.0]]
+let v2 = m1 * v1                    //矩阵和向量相乘
+let m2 = Matrix.transpose m1        //矩阵转置
+let m3 = m1 * m2                    //矩阵和矩阵相乘
+printfn "%O" v2
+printfn "%O" m3
