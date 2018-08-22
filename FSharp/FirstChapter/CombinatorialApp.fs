6c48d88a29c67ff6fd6b8812bd84095fb1a44dc3
LoveLan1314 <736265181@qq.com>
2018-08-23 00:03:25 +0800

    1.5

---
+++ FSharp/FirstChapter/CombinatorialApp.fs
@@ -0,0 +1,13 @@
+﻿module CombinatorialApp
+
+let rec mss x c = 
+    if List.length x = 0 then 0
+    else
+        if x.Head>c then mss x.Tail c
+        else max (mss x.Tail c) (x.Head + (mss x.Tail (c-x.Head)))
+
+let x = [2; 3; 5; 10; 15; 20; 25]
+printfn "%i" (mss x 49)
+
+let y = [11..16]
+printf "%i" (mss y 49)
---
+++ FSharp/FirstChapter/ConsoleInApp.fs
@@ -0,0 +1,11 @@
+﻿module ConsoleInApp
+
+open System
+
+Console.WriteLine "请输入密码："
+let s = Console.ReadLine()
+if s = "888" then 
+    let a = Console.Read()
+    Console.WriteLine a
+else
+    Console.WriteLine "密码错误"
