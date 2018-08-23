9f8be6983c882cc79e4acc33b897743b55b502a3
LoveLan1314 <736265181@qq.com>
2018-08-23 22:26:54 +0800

    第1章

--- FSharp/FirstChapter/BasicApp.fs
+++ FSharp/FirstChapter/BasicApp.fs
@@ -5,6 +5,16 @@ let fangle a b c =
     let s = sqrt (l*(l-a)*(l-b)*(l-c))
     asin (2.0*s/b/c), asin (2.0*s/a/c), asin (2.0*s/a/b)
 
+let fangle1 a b c = 
+    let l = (a+b+c)/2.0
+    let s = sqrt (l*(l-a)*(l-b)*(l-c))
+    asin (2.0*s/b/c) /Complex.pi.r*180.0, asin (2.0*s/a/c) /Complex.pi.r*180.0, 
+    asin (2.0*s/a/b) /Complex.pi.r*180.0
+
 let q1 = fangle 3.0 4.0 5.0;;
 
 let q2 = fangle 1.5 1.0 1.5;;
+
+let q3 = fangle1 3.0 4.0 5.0;;
+
+let q4 = fangle1 1.5 1.0 1.5;;
--- FSharp/FirstChapter/DerivativeApp.fs
+++ FSharp/FirstChapter/DerivativeApp.fs
@@ -15,3 +15,19 @@ let f x = exp (x-1.0/x)
 printfn "f=%f, f_1=%f" (f 1.5) (Deriative f 1.5);;
 
 for i=1 to 5 do printf "f_%i=%f, " i (MDeriative f i 1.5);;
+
+let Deriative1 f x = 
+    let p = f (x + e)
+    let q = f (x - e)
+    (p - q) / (2.0*e)
+
+let rec MDeriative1 f n x = 
+    if n = 0 then f x
+    else 
+        let p = MDeriative1 f (n-1) (x+e)
+        let q = MDeriative1 f (n-1) (x-e)
+        (p-q) / (2.0*e)
+
+printfn "f=%f, f_1=%f" (f 1.5) (Deriative1 f 1.5);;
+
+for i=1 to 5 do printf "f_%i=%f, " i (MDeriative1 f i 1.5);;
