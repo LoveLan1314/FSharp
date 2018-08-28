1ea0be85887f920a81c856006c53515320b2e2b1
LoveLan1314 <736265181@qq.com>
2018-08-28 23:33:10 +0800

    2.2.1

---
+++ FSharp/SecondChapter/BaseValueType.fs
@@ -0,0 +1,85 @@
+﻿module File1
+
+let b = true
+printfn "%b" b;;
+
+let b1 = true
+let b2 = not b1;;
+
+let g a b = (a || b) && not (a && b)
+printf "%b" (g b1 b2);;
+
+let i1 = 300;;
+
+let i2 = 300s;;
+
+let i3 = 300 - 200;;
+
+let i4 = -i3;;
+
+let i5 = 300 / 200;;
+
+let i6 = 300 % 200;;
+
+//let i7 = 128y;;
+
+let i8 = 127y + 1y;;
+
+let i9 = 300us * 300us;;
+
+open Microsoft.FSharp.Core.Operators.Checked
+let i10 = 127y + 1y;;
+let i11 = 300us * 300us;;
+
+let f1 = 1.0f;;
+
+let d1 = 3.14;;
+
+let m1 = 0.25M;;
+
+let x = 6.0 % 2.5;;
+
+let y = 7.5 % 3.0;;
+
+let f2 = 0.4e12 + 1.5e11;;
+
+let f3 = 0.4e12 * 1.5e11;;
+
+let z = 1000.0 / 0.3
+printfn "%f" z;;
+
+printfn "%.3f" z;;
+
+printfn "%e" z;;
+
+printfn "%.2e" z;;
+
+let z1 = 97.5 + infinity;;  // 一般数加上infinity仍是infinity
+
+let z2 = 1.0 - z1;;   // 一般数减去infinity仍是infinity
+
+let z3 = -2.0 * z2;;    // 一般数乘以infinity也是infinity，符合一般乘法规则
+
+let z4 = -2.0 / -infinity;; // 一般数除以infinity为0
+
+let z5 = 5.0 / 0.0;;
+
+let z6 = 0.0 / 0.0;;
+
+let f x y = 
+    if x > 0.0 && x <= 1.0 then (1.0 - x) / x
+    else nan
+
+let c = 'a';;
+
+let s = "apple";;
+
+let s' = "微软Office";;
+
+printfn "%s" "C:\\Program Files\\JOS\\版权声明.txt\n\t\"UNMSG\"是一款完全免费的局域网即时通信工具\v可通过邮件注册。";;
+
+let s1 = "师范大学"
+let c1 = s1.[3];;
+
+let s2 = "本科生"
+let s3 = s1 + " " + s2;;
---
+++ FSharp/SecondChapter/ComplexType.fs
@@ -0,0 +1,39 @@
+﻿module ComplexType
+
+let a = (1.0, 0.5);;
+
+let b = (170, 65.5);;
+
+let s1 = ("王霞", 158, 51.2);;
+
+let s2 = ("李明", b);;
+
+let x = fst a;;
+
+let y = snd a + snd b;;
+
+let n, h, w = s1;;
+
+//let p, q = s1;;
+
+let name, _, _ = s1;;
+
+let z = (2, 3.14)
+printfn "(%i, %f)" (fst z) (snd z);;
+
+printfn "%O" z;;
+
+let a1, a2, a3 = 3, 6, 7.5;;
+
+let maxmin a b c =
+    if a>b then
+        if b>c then (a,c)
+        else if a>c then (a,b)
+        else (c,b)
+    else
+        if b<c then (c,a)
+        else if a<c then (b,a)
+        else (b,c)
+let r = maxmin 2.0 5.0 4.0;;
+
+printfn "max: %f, min: %f" (fst r) (snd r);;
